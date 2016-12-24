7c83c4ffc172c5cd66245605b324e652a2ef5183
alejandro <alejandro.aleman.hernandez@gmail.com>
2016-12-24 10:49:47 -0600

    added dynamic links

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const express = require('express');
 
+const linksObject = require('./links-object.js').linksObject;
+
 const app = express();
 
 app.use('/static/', express.static(path.join(__dirname, '\\public')));
@@ -10,7 +12,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '\\views'));
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', linksObject);
 });
 
 app.listen(8001);
---
+++ links-object.js
@@ -0,0 +1,8 @@
+const linksObject = {
+	linksArray: [
+		// add new paths on here
+		{"href": "bunny", "label":"bunny"}
+	]
+}
+
+module.exports.linksObject = linksObject;
