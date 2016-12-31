bb2ea04bb51913bdc57e8e300fed721c9e4f6b74
alejandro <alejandro.aleman.hernandez@gmail.com>
2016-12-31 14:57:46 -0600

    draggable prototype

--- public/js/mandelbrot.js
+++ public/js/mandelbrot.js
@@ -1,24 +1,40 @@
 // author: Daniel Shiffman
 // source: https://www.youtube.com/watch?v=6z7GQewK-Ks
 
+let lastX;
+let lastY;
+let currentX;
+let currentY;
+
 function setup() {
 	createCanvas(360, 240);
 	pixelDensity(1);
+	lastX = 0;
+	lastY = 0;
+	currentX = 0;
+	currentY = 0;
 }
 
 function draw() {
-	const iterations = 200;
+	console.log(lastX);
+	console.log(lastY);
+	console.log(currentX);
+	console.log(currentY);
+
+	const iterations = 20;
 	const minX = -2;
 	const maxX = 1;
 	const minY = -1;
 	const maxY = 1;
+	const ratioX = (maxX - minX) / width;
+	const ratioY = (maxY - minY) / height;
 
 	loadPixels();
 
 	for(let x = 0; x < width; x++) {
 		for(let y = 0; y < height; y++) {
-			let a = map(x, 0, width, minX, maxX);
-			let b = map(y, 0, height, minY, maxY);
+			let a = map(x, 0, width, minX + (lastX - currentX) * ratioX, maxX + (lastX - currentX) * ratioX);
+			let b = map(y, 0, height, minY + (lastY - currentY) * ratioY, maxY + (lastY - currentY) * ratioY);
 			const constA = a;
 			const constB = b;
 			let n = 0;
@@ -58,4 +74,19 @@ function draw() {
 	}
 
 	updatePixels();
+}
+
+function mousePressed() {
+	noLoop();
+
+	setTimeout(() => {
+		lastX = mouseX;
+		lastY = mouseY;
+	}, 0)
+}
+
+function mouseReleased() {
+	currentX = mouseX;
+	currentY = mouseY;
+	loop();
 }
