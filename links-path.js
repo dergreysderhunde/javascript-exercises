7a8511fb2ba377d19e3341e435cf3a24aeff6b75
alejandro <alejandro.aleman.hernandez@gmail.com>
2017-01-01 21:34:12 -0600

    added JSONbot

--- links-path.js
+++ links-path.js
@@ -12,8 +12,30 @@ function paths(app, linksObject) {
 	});
 
 	app.get('/jsonbot', (req, res) => {
+		const Twit = require('twit');
+		const config = require('./config.json');
+		const T = new Twit(config);
+
 		res.render('jsonbot');
-	})
+
+		function bot() {
+			const date = new Date();
+			const seconds = date.getUTCSeconds();
+			const milliseconds = date.getUTCMilliseconds();
+			const random = Math.random() * seconds * (milliseconds / 1000);
+
+			T.post('statuses/update', {status: `{\n  "random": ${random}\n}`}, function (err, data, res) {
+				if (err) {
+					console.log(err);
+				}
+				if (data && res) {
+					console.log(`tweet'd:\n  {\n    "random": ${random}\n  }`);
+				}
+			});
+		}
+		bot();
+		setInterval(bot, 1000 * 60); // tweets each minute
+	});
 }
 
 module.exports.paths = paths;
