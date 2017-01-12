15fd539efebc7ac117701ef30f7f15cd19ff470a
alejandro <alejandro.aleman.hernandez@gmail.com>
2017-01-11 20:37:14 -0600

    start from zero

--- requires/links-object.js
+++
@@ -1,9 +0,0 @@
-const linksObject = {
-	linksArray: [
-		// add new paths on here
-		{href: 'jsonbot', label: 'JSONbot'},
-		{href: 'mandelbrot', label: 'mandelbrot'}
-	]
-};
-
-module.exports.linksObject = linksObject;
