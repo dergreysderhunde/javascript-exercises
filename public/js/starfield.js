91a81e2518e1f86cf360066e8547a6155f6b9c78
alejandro <alejandro.aleman.hernandez@gmail.com>
2017-01-11 22:00:43 -0600

    started starfield with p5.js

---
+++ public/js/starfield.js
@@ -0,0 +1,8 @@
+function setup() {
+	createCanvas(document.body.offsetWidth, document.body.offsetHeight);
+	background(0);
+}
+
+function draw() {
+
+}
--- requires.js
+++ requires.js
@@ -4,5 +4,7 @@ module.exports.columns = columns;
 
 const links = [
 	// put your links on here!
-	{path: "path1", label: "label1"}
+	{path: "starfield", label: "starfield"}
 ]
+
+module.exports.links = links
