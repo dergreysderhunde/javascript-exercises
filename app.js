ae996308b52a3243732ddd4793e5acc921d40bfc
alejandro <alejandro.aleman.hernandez@gmail.com>
2017-01-13 20:36:09 -0600

    ???

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
+	console.log("GET request to main app");
 	res.render('index', {
 		links: links,
 		columns: columns
@@ -18,10 +19,11 @@ app.get('/', (req, res) => {
 
 for (let i = 0; i < links.length; i++) {
 	app.get('/' + links[i].path, (req, res) => {
+		console.log(`GET request to ${links[i].label} app`);
 		res.render(links[i].path + '.pug');
 	});
 }
 
 app.listen(8001, () => {
-	console.log('app running on http://127.0.0.1:8001');
+	console.log('App running on http://127.0.0.1:8001');
 });
